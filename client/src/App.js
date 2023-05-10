909dfc9aba31cb63d4b5fdd093220e8cfc7ad88b
Julien Quatravaux <57346817+Julien-QTX@users.noreply.github.com>
2023-05-10 17:31:16 +0200

    les bases du projet a revoire

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    ).catch(
+      error => console.error('Failed to retrieve API data', error)
+    )
+  }, [])
+
+
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{JSON.stringify(user)}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
